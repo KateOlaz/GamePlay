37136a11182d68451d6833eb95d6f026943af8ed
KateOlaz <49486445+KateOlaz@users.noreply.github.com>
2019-06-01 00:22:19 -0500

    Update juego.cpp

--- juego.cpp
+++ juego.cpp
@@ -53,6 +53,7 @@ void juego::cargar_graficos()
     }
 }
 
+//Cargo lo necesario para mis personajes 
 void juego::personajes()
 {
     textura1 = new Texture;
@@ -77,6 +78,8 @@ void juego::personajes()
 
 }
 
+//Esta funcion divide mi textura en diferentes rectuangulos, devolviendome en que yo quiero especificamente
+//(int x,int y)son la pocicion del rectangulo en especifico que requiero 
 void juego::frames(Sprite &sprite, int x, int y, int num_x, int num_y)
 {
     IntRect posicion(x * (sprite.getTexture()->getSize().x / num_x),y * (sprite.getTexture()->getSize().y/ num_y),sprite.getTexture()->getSize().x / num_x, sprite.getTexture()->getSize().y / num_y);
